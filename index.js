65d8add1a55d0fff2cba3b91a20880a4c35d8ffc
Ayfri <pierre.ayfri@gmail.com>
2020-03-21 22:52:42 +0100

    Added files.

---
+++ index.js
@@ -0,0 +1,73 @@
+const {Client, MessageEmbed} = require('discord.js');
+const GodEmbot = new Client();
+const {token, example, explication} = require('statics.json');
+const toEmbed = require('godembed');
+const prefix = 'embed.';
+
+GodEmbot.login(token).catch((err) => {
+	console.log(err);
+});
+
+GodEmbot.once('ready', () => {
+	console.log('Bot Ready !');
+});
+
+GodEmbot.on('message', async message => {
+	if (message.system || message.author === GodEmbot.user || !message.content.startsWith(prefix)) {
+		return false;
+	}
+	
+	if (message.guild) {
+		const permissions = message.guild.me.permissions;
+		if ( !permissions.has('SEND_MESSAGES')) {
+			if (permissions.has('ADD_REACTIONS')) {
+				await message.react('❌');
+				await message.react('✍️');
+			} else {
+				await message.author.send('Hi, I\'m here to say to you that I can\'t send messages, I don\'t have the proper permission, you can say it to the owner, or an admin to help me gain missing permissions ^^\'');
+			}
+		}
+	}
+	
+	if (message.content.startsWith(prefix)) {
+		const args = message.content.slice(prefix.length).trim().split(/\s+/g);
+		
+		switch (args[0]) {
+			case 'create':
+			case 'new':
+				const {embed, errors} = toEmbed(args.slice(1).join(' '));
+				if (embed.length > 0) {
+					await message.channel.send({embed});
+				}
+				if (errors.length > 0) {
+					await message.channel.send(`\`\`\`js\n${errors.join('\n')}\`\`\``);
+				}
+				
+				break;
+			
+			case 'help':
+				const helpEmbed = new MessageEmbed();
+				helpEmbed.setAuthor('God Embot a bot to help you create embeds !');
+				helpEmbed.setTitle('Bot Help');
+				helpEmbed.setColor('#ff9900');
+				helpEmbed.setFooter(`Do ${prefix}example to get an example !`);
+				helpEmbed.addField('Commands', `
+\`${prefix}help\` - Show you this help.
+\`${prefix}example\` - Show you an example.
+\`${prefix}new\` - Let you create a new embed.`);
+				
+				helpEmbed.addField('How this works ?', explication);
+				
+				await message.channel.send(helpEmbed);
+				break;
+			
+			case 'example':
+			case 'exemple':
+				const textEmbed = toEmbed(example);
+				
+				await message.channel.send(`\`\`\`pf\n${example}\`\`\``);
+				await message.channel.send({embed: textEmbed.embed});
+				break;
+		}
+	}
+});
